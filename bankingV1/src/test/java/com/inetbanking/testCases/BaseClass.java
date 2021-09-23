6a8a90c3b4dbee1044af0b978a9afc4fc7c0cf8f
dinithiMW <dinithimweerasinghe95@gmail.com>
2021-09-23 20:07:56 +0530

    updated

--- bankingV1/src/test/java/com/inetbanking/testCases/BaseClass.java
+++ bankingV1/src/test/java/com/inetbanking/testCases/BaseClass.java
@@ -4,6 +4,9 @@ import org.openqa.selenium.WebDriver;
 import org.testng.annotations.AfterClass;
 import org.testng.annotations.BeforeClass;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.apache.log4j.Logger;
+import org.apache.log4j.PropertyConfigurator;
+
 
 public class BaseClass {
 	
@@ -12,10 +15,15 @@ public class BaseClass {
 	public String password="YvuzYtY";
 	public static WebDriver driver;
 	
+	public static Logger logger;
+	
 	@BeforeClass
 	public void setup() {
 		System.setProperty("webdriver.chrome.driver",System.getProperty("user.dir")+"//Drivers//chromedriver.exe");
 		driver= new ChromeDriver();
+		
+		logger = Logger.getLogger("ebanking");
+		PropertyConfigurator.configure("Log4j.properties");		
 		
 	}
 	
--- bankingV1/src/test/java/com/inetbanking/testCases/TC_LoginTest_001.java
+++ bankingV1/src/test/java/com/inetbanking/testCases/TC_LoginTest_001.java
@@ -9,18 +9,24 @@ public class TC_LoginTest_001 extends BaseClass {
 	@Test
 	public void loginTest() {
 		driver.get(baseURL);
+		logger.info("URL is opened");
 		
 		LoginPage lp=new LoginPage(driver);
 		lp.setUsername(username);
+		logger.info("Entered Username");
+		
 		lp.setPassword(password);
+		logger.info("Entered Password");
 		
 		lp.clickSubmit();
 		
 		if(driver.getTitle().equals("Guru99 Bank ManageHomePage")) {
 			Assert.assertTrue(true);
+			logger.info("Login Test Passed");
 		}
 		else {
 			Assert.assertFalse(false);
+			logger.info("Login Test Failed");
 		}
 		
 		
